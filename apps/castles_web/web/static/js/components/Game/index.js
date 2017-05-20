7d19ca0f43c9105c569e60711d4dfd0c64d41307
Szymon Janota <mr.akra@gmail.com>
2017-05-20 16:39:15 +0200

    sjanota: picking units; game styling

--- apps/castles_web/web/static/js/components/Game/index.js
+++ apps/castles_web/web/static/js/components/Game/index.js
@@ -1,39 +1,88 @@
 import React from 'react'
+import { Debug } from '../Common'
 import { capitalize } from '../../util'
+import classNames from 'classnames'
 
-const player1 = {name: 'Karo', defence:["archer", "axeman", "knight", "archer", "viking"]}
-const player2 = {name: 'CJ', defence: Array(5).fill("unknown")}
-
-const activePlayer = player1.name;
 const initArmy = Array(5).fill("empty");
-const allUnits = ["archer", "axeman", "knight", "ninja", "shieldbearer", "swordsman", "viking"];
+
+const player1 = {
+  name: 'Karo',
+  defence:["archer", "axeman", "knight", "archer", "viking"],
+  offence: initArmy,
+  color: 'red'
+};
+const player2 = {
+  name: 'CJ',
+  defence: Array(5).fill("unknown"),
+  offence: initArmy,
+  color: 'blue'
+};
+
+const colors = {
+  red: 'rgba(255,0,0,0.3)',
+  blue: 'rgba(0,0,255,0.3)',
+}
+
+const allUnits = ["archer", "axeman", "knight", "ninja", "shieldbearer", "swordsman", "viking", "empty"];
 
 class GameStatus extends React.Component {
   render() {
     return (
       <div className="status">
-        <p>{this.props.activePlayer}'s turn</p>
+        <p>{this.props.activePlayer.name}'s turn</p>
       </div>
     );
   }
 }
 
-class Army extends React.Component {
+class Controls extends React.Component {
   render() {
     return (
-      <div className={"army my-column" + (this.props.hidden ? " not-shown" : "")}>
-        {this.props.units.map((unit, i) => {
-          const capitalizedUnit = capitalize(unit);
-          return (<div className={"unit " + (unit === "unknown" ? "no-mirror" : "")} key={i}>
-            <img
-              width="50"
-              height="50"
-              src={"images/" + unit + ".png"}
-              alt={capitalizedUnit}
-              title={capitalizedUnit}
-            />
-          </div>);
-        })}
+      <div className="controls">
+        <button
+          onClick={this.props.onAttack}
+          disabled={!this.props.isAttackPossible}
+        >Attack!</button>
+      </div>
+    );
+  };
+}
+
+class Unit extends React.Component {
+  render() {
+    const capitalizedUnit = capitalize(this.props.unit);
+    const unitClasses = classNames('unit', {
+      'no-mirror': this.props.unit === "unknown"
+    });
+    return (
+      <div className={unitClasses}>
+        <img
+          onClick={this.props.onClick}
+          width="50"
+          height="50"
+          src={"images/" + this.props.unit + ".png"}
+          alt={capitalizedUnit}
+          title={capitalizedUnit}
+        />
+      </div>
+    )
+  }
+}
+
+class Army extends React.Component {
+  render() {
+    const armyClasses = classNames('army', 'my-column', {
+      'not-shown': !this.props.isShown,
+      editable: this.props.isEditable
+    });
+    return (
+      <div className={armyClasses}>
+        {this.props.units.map((unit, i) =>
+          <Unit key={i}
+            unit={unit}
+            onClick={() => this.props.onUnitClick(unit, i)}
+          />
+        )}
       </div>
     );
   }
@@ -41,43 +90,155 @@ class Army extends React.Component {
 
 class UnitPicker extends React.Component {
   render() {
+    const classes = classNames('unit-picker', {
+      'not-shown': !this.props.isShown
+    });
     return (
-      <div className={"unit-picker " + (this.props.isActive ? "" : "not-shown")}>
+      <div className={classes}>
         <p>Choose unit</p>
-        <Army units={allUnits} hidden={!this.props.isActive}/>
+        <Army
+          units={allUnits}
+          isShown={this.props.isShown}
+          isEditable={false}
+          onUnitClick={this.props.onUnitClick}
+        />
       </div>
     );
   }
+}
+
+function PlayerName(props) {
+  return (
+    <div className="name">
+      <p className="align">{props.player.name}</p>
+      <p className="align">({props.title})</p>
+    </div>
+  )
 }
 
 class Base extends React.Component {
   render() {
+    const baseClasses = classNames('player-base', this.props.side)
     return (
-      <div className={"player-base " + this.props.side}>
-        <p className="align name">{this.props.player.name}</p>
-        <div className="my-container">
-          <span>Defence</span><span>Attack</span>
+      <div className={baseClasses} style={{
+        backgroundColor: colors[this.props.player.color]
+      }}>
+        <div className="header">
+          <PlayerName
+            player={this.props.player}
+            title={this.props.title}
+          />
+          <div className="my-container">
+            <span>Defence</span><span>Attack</span>
+          </div>
         </div>
-        <div className="my-container">
-          <Army units={this.props.player.defence}/>
+        <div className="my-container main">
+          <Army
+            units={this.props.player.defence}
+            isShown={true}
+            isEditable={this.props.isDefenceEditable}
+            onUnitClick={() => {}}
+          />
           <div><img src="images/castle-vertical.png" className="castle"/></div>
-          <Army units={initArmy} hidden={!this.props.myTurn}/>
+          <Army
+            units={this.props.player.offence}
+            isShown={this.props.isOffenceShown}
+            isEditable={this.props.isOffenceEditable}
+            onUnitClick={this.props.onUnitClick}
+          />
         </div>
       </div>
     );
   }
 }
 
-export function Game(props) {
-  return (
-    <div className="game">
-      <h3>Game</h3>
-      <GameStatus activePlayer={activePlayer}/>
-      <div className="board my-container">
-        <Base player={player1} side="left" myTurn={player1.name === activePlayer}/>
-        <UnitPicker isActive={true}/>
-        <Base player={player2} side="right"  myTurn={player2.name === activePlayer}/>
+export class Game extends React.Component {
+  constructor() {
+    super();
+    this.switchActivePlayer = this.switchActivePlayer.bind(this);
+    this.onSlotSelected = this.onSlotSelected.bind(this);
+    this.onUnitSelected = this.onUnitSelected.bind(this);
+    this.onAttack = this.onAttack.bind(this);
+    this.state = {
+      myPlayer: player1,
+      opponentPlayer: player2,
+      myTurn: true,
+      unitPickerActive: false,
+      selectedSlot: null
+    };
+  };
+
+  render() {
+    return (
+      <div className="game">
+        <h3>Game</h3>
+        <GameStatus activePlayer={this.activePlayer()}/>
+        <Controls
+          isAttackPossible={this.isAttackPossible()}
+          onAttack={this.onAttack}
+        />
+        <div className="board my-container">
+          <Base
+            player={this.state.myPlayer}
+            title="You"
+            side="left"
+            isDefenceEditable={false}
+            isOffenceEditable={this.state.myTurn}
+            isOffenceShown={this.state.myTurn}
+            onUnitClick={this.onSlotSelected}
+          />
+          <UnitPicker
+            isShown={this.state.selectedSlot != null}
+            onUnitClick={this.onUnitSelected}
+          />
+          <Base
+            player={this.state.opponentPlayer}
+            title="Opponent"
+            side="right"
+            isDefenceEditable={false}
+            isOffenceEditable={false}
+            isOffenceShown={true}
+            onUnitClick={() => {}}
+          />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  };
+
+  activePlayer() {
+    return this.state.myTurn ? this.state.myPlayer : this.state.opponentPlayer;
+  };
+
+  switchActivePlayer() {
+    this.setState((prev) => prev.myTurn = !prev.myTurn);
+  };
+
+  onSlotSelected(unit, i) {
+    this.setState({selectedSlot: i});
+  };
+
+  onUnitSelected(unit, i) {
+    const player = Object.assign({}, this.activePlayer());
+    const offence = player.offence.slice();
+    offence[this.state.selectedSlot] = unit;
+    player.offence = offence;
+    this.setActivePlayer(player)
+    this.setState({selectedSlot: null})
+  };
+
+  setActivePlayer(player) {
+    if (this.state.myTurn) {
+      this.setState({myPlayer: player})
+    } else {
+      this.setState({opponentPlayer: player})
+    }
+  };
+
+  isAttackPossible() {
+    return this.state.myPlayer.offence.every((it) => it !== "empty")
+  };
+
+  onAttack() {
+
+  };
+};
